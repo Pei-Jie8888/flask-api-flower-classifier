e59c81e89dfdc3527b37291930dc55a9e93a55c6
Pei-Jie <pjwang8888@gmail.com>
2021-07-04 23:04:26 +0800

    flower classifier

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import app.model as model
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/test',  methods=['GET'])
+def getResult():
+    input np.array([[5.5,2.4,2.7,1.]])
+    result = model.predict(input)
+    return jsonify({'return': str(result)})
+
+@app.route('/predict', methods=['POST'])
+def postInput():
+    # 取得前端傳過來的數值
+    insertValues = request.get_json()
+    x1=insertValues['sepalLengthCm']
+    x2=insertValues['sepalWidthCm']
+    x3=insertValues['petalLengthCm']
+    x4=insertValues['petalWidthCm']
+    input = np.array([[x1, x2, x3, x4]])
+
+    result = model.predict(input)
+
+    return jsonify({'return': str(result)})
